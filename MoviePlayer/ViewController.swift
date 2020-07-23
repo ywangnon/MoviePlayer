7f67e4cecfbf719f4798f49dbbe33b60be60e944
ywangnon <ywangnon123@gmail.com>
2020-07-23 23:21:08 +0900

    프로젝트 생성

---
+++ MoviePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by Hansub Yoo on 2020/07/23.
+//  Copyright © 2020 Hansub Yoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
